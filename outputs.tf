869297493a878d03be09cc48685a5ca55e72280a
sasa <sasa.popravak@gmail.com>
2022-11-01 19:43:15 +0100

    Outputs changes

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,14 @@
-
+/*
 output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
 
 output "snet_name" {
   value = azurerm_virtual_network.vnet.subnet
+}
+
+*/
+
+output "snet_id" {
+  value = azurerm_subnet.snet.id
 }
