2b05abecf358f664d66fa4a4f131d0fa1b2fe92f
sasa <sasa.popravak@gmail.com>
2022-11-01 21:11:35 +0100

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,13 @@ output "snet_name" {
   value = azurerm_virtual_network.vnet.subnet
 }
 
-*/
 output "snet_id" {
   value = values(azurerm_subnet.snet).*.id
+}
+
+*/
+
+output "subnet_ids" {
+  description = "List of subnets IDs"
+  value       = flatten(concat([for s in azurerm_subnet.snet : s.id]))
 }
